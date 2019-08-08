1239fcd91d7b2237177f3eea728ee34f0672d37d
m4nzm333 <irman.mashuri@gmail.com>
2019-08-09 01:45:11 +0800

    Migrasi ke MySQL

--- application/models/PasienModel.php
+++ application/models/PasienModel.php
@@ -2,10 +2,51 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class PasienModel extends CI_Model {
+  // SQL Lite
+  // public function insert_pasien($data)
+  // {
+  //   $this->db->set('created_on', '\''.$this->get_now().'\'', FALSE);
+  //   $this->db->set('last_update', '\''.$this->get_now().'\'', FALSE);
+  //   $this->db->insert('pasien', $data);
+  //   return $this->db->insert_id();
+  // }
+  // public function get_pasien_all()
+  // {
+  //   return $this->db->get('pasien')->result_array();
+  // }
+  // public function get_pasien_by_id($id_pasien)
+  // {
+  //   $this->db->where('id_pasien', $id_pasien);
+  //   return $this->db->get('pasien')->row_array();
+  // }
+  // public function update_pasien_by_id($id_pasien, $data)
+  // {
+  //   $this->db->set('last_update', '\''.$this->get_now().'\'', FALSE);
+  //   $this->db->where('id_pasien', $id_pasien);
+  //   $this->db->update('pasien', $data);
+  //   return $this->db->affected_rows();
+  // }
+  // public function delete_pasien_by_id($id_pasien)
+  // {
+  //   $this->db->where('id_pasien', $id_pasien);
+  //   $this->db->delete('pasien');
+  //   return $this->db->affected_rows();
+  // }
+  // public function get_count_pasien()
+  // {
+  //   return $this->db->get('pasien')->num_rows();
+  // }
+  //
+  // public function get_now()
+  // {
+  //   date_default_timezone_set('Asia/Makassar');
+  //   $date = new DateTime();
+  //   return date_format($date, 'Y-m-d H:m:s');
+  // }
   public function insert_pasien($data)
   {
-    $this->db->set('created_on', '\''.$this->get_now().'\'', FALSE);
-    $this->db->set('last_update', '\''.$this->get_now().'\'', FALSE);
+    $this->db->set('created_on', 'NOW()', FALSE);
+    $this->db->set('last_update', 'NOW()', FALSE);
     $this->db->insert('pasien', $data);
     return $this->db->insert_id();
   }
@@ -20,7 +61,7 @@ class PasienModel extends CI_Model {
   }
   public function update_pasien_by_id($id_pasien, $data)
   {
-    $this->db->set('last_update', '\''.$this->get_now().'\'', FALSE);
+    $this->db->set('last_update', 'NOW()', FALSE);
     $this->db->where('id_pasien', $id_pasien);
     $this->db->update('pasien', $data);
     return $this->db->affected_rows();
@@ -34,12 +75,5 @@ class PasienModel extends CI_Model {
   public function get_count_pasien()
   {
     return $this->db->get('pasien')->num_rows();
-  }
-
-  public function get_now()
-  {
-    date_default_timezone_set('Asia/Makassar');
-    $date = new DateTime();
-    return date_format($date, 'Y-m-d H:m:s');
   }
 }
--- application/models/PerawatanModel.php
+++ application/models/PerawatanModel.php
@@ -2,10 +2,82 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class PerawatanModel extends CI_Model {
+  // SQLite
+  // public function insert_perawatan($data)
+  // {
+  //   $this->db->set('created_on', '\''.$this->get_now().'\'', FALSE);
+  //   $this->db->set('last_update', '\''.$this->get_now().'\'', FALSE);
+  //   $this->db->insert('perawatan', $data);
+  //   return $this->db->insert_id();
+  // }
+  // public function get_perawatan_by_id_pasien($id_pasien)
+  // {
+  //   $this->db->where('id_pasien', $id_pasien);
+  //   return $this->db->get('perawatan')->result_array();
+  // }
+  // public function get_perawatan_by_id($id_perawatan)
+  // {
+  //   $this->db->where('id_perawatan', $id_perawatan);
+  //   return $this->db->get('perawatan')->row_array();
+  // }
+  // public function update_perawatan_by_id($id_perawatan, $data)
+  // {
+  //   $this->db->set('last_update', '\''.$this->get_now().'\'', FALSE);
+  //   $this->db->where('id_perawatan', $id_perawatan);
+  //   $this->db->update('perawatan', $data);
+  //   return $this->db->affected_rows();
+  // }
+  // public function delete_perawatan_by_id($id_perawatan)
+  // {
+  //   $this->db->where('id_perawatan', $id_perawatan);
+  //   $this->db->delete('perawatan');
+  //   return $this->db->affected_rows();
+  // }
+  // public function get_count_perawatan()
+  // {
+  //   $this->db->select('sum(biaya) as total');
+  //   $data = $this->db->get('perawatan')->row_array();
+  //   return $data['total'];
+  // }
+  //
+  // public function get_count_perawatan_where($tahun = null, $bulan = null)
+  // {
+  //   $this->db->select('id_perawatan, nama, diagnosa, terapi, biaya, tanggal, alamat, substr(tanggal, 1, 4) as tahun, substr(tanggal, 6, 2) as bulan, biaya');
+  //   if ($tahun != null) {
+  //     $this->db->where('tahun', $tahun);
+  //   }
+  //   if ($bulan != null) {
+  //     $this->db->where('bulan', $bulan);
+  //   }
+  //   $this->db->join('pasien', 'perawatan.id_pasien = pasien.id_pasien', 'left');
+  //   return $this->db->get('perawatan')->result_array();
+  // }
+  // public function get_count_perawatan_by_id_pasien($id_pasien)
+  // {
+  //   $this->db->select('sum(biaya) as total');
+  //   $this->db->where('id_pasien', $id_pasien);
+  //   $data =  $this->db->get('perawatan')->row_array();
+  //   return $data['total'];
+  // }
+  // public function get_count_perawatan_by_bulan()
+  // {
+  //   $this->db->select('(substr(tanggal, 1, 4)) || "-" ||(substr(tanggal, 6, 2)) as bulan, sum(biaya) as total');
+  //   $this->db->group_by('tahun, bulan');
+  //   $this->db->order_by('tahun', 'ASC');
+  //   $this->db->order_by('bulan', 'ASC');
+  //   return $this->db->get('perawatan')->result_array();
+  // }
+  //
+  // public function get_now()
+  // {
+  //   date_default_timezone_set('Asia/Makassar');
+  //   $date = new DateTime();
+  //   return date_format($date, 'Y-m-d H:m:s');
+  // }
   public function insert_perawatan($data)
   {
-    $this->db->set('created_on', '\''.$this->get_now().'\'', FALSE);
-    $this->db->set('last_update', '\''.$this->get_now().'\'', FALSE);
+    $this->db->set('created_on', 'NOW()', FALSE);
+    $this->db->set('last_update', 'NOW()', FALSE);
     $this->db->insert('perawatan', $data);
     return $this->db->insert_id();
   }
@@ -21,7 +93,7 @@ class PerawatanModel extends CI_Model {
   }
   public function update_perawatan_by_id($id_perawatan, $data)
   {
-    $this->db->set('last_update', '\''.$this->get_now().'\'', FALSE);
+    $this->db->set('last_update', 'NOW()', FALSE);
     $this->db->where('id_perawatan', $id_perawatan);
     $this->db->update('perawatan', $data);
     return $this->db->affected_rows();
@@ -41,7 +113,7 @@ class PerawatanModel extends CI_Model {
 
   public function get_count_perawatan_where($tahun = null, $bulan = null)
   {
-    $this->db->select('id_perawatan, nama, diagnosa, terapi, biaya, tanggal, alamat, substr(tanggal, 1, 4) as tahun, substr(tanggal, 6, 2) as bulan, biaya');
+    $this->db->select('id_perawatan, nama, diagnosa, terapi, biaya, tanggal, alamat, YEAR(tanggal) as tahun, MONTH(tanggal) as bulan, biaya');
     if ($tahun != null) {
       $this->db->where('tahun', $tahun);
     }
@@ -60,17 +132,10 @@ class PerawatanModel extends CI_Model {
   }
   public function get_count_perawatan_by_bulan()
   {
-    $this->db->select('(substr(tanggal, 1, 4)) || "-" ||(substr(tanggal, 6, 2)) as bulan, sum(biaya) as total');
+    $this->db->select('YEAR(TANGGAL) as tahun,YEAR(tanggal) || "-" || MONTH(tanggal) as bulan, sum(biaya) as total');
     $this->db->group_by('tahun, bulan');
     $this->db->order_by('tahun', 'ASC');
     $this->db->order_by('bulan', 'ASC');
     return $this->db->get('perawatan')->result_array();
-  }
-
-  public function get_now()
-  {
-    date_default_timezone_set('Asia/Makassar');
-    $date = new DateTime();
-    return date_format($date, 'Y-m-d H:m:s');
   }
 }
